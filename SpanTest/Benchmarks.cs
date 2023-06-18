9eaeb758bfbc53288cd40308eb57793364e63259
Harsh Bothra <harsh.love.619@gmail.com>
2023-06-19 02:00:22 +0530

    Add project files.

---
+++ SpanTest/Benchmarks.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Tracing.AutomatedAnalysis;
+using System.Runtime.InteropServices;
+
+namespace SpanTest
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        private static readonly Random _random = new(8000);
+
+        [Params(100, 100_000, 1_000_000)]
+        public int Size { get; set; }
+
+        private List<int> _items;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _items = Enumerable.Range(1, Size).Select(x => _random.Next()).ToList();
+        }
+
+        [Benchmark]
+        public void For()
+        {
+            for (int i = 0; i < _items.Count; i++)
+            {
+                _items[i] = _items[i] * 2;
+            }
+        }
+
+        [Benchmark]
+        public void Span()
+        {
+            var spans = CollectionsMarshal.AsSpan(_items);
+            for (int i=0; i < spans.Length; i++)
+            {
+                spans[i] = spans[i] * 2;
+            }
+        }
+    }
+}
---
+++ SpanTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace SpanTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
